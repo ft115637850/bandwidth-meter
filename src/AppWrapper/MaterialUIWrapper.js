91a3ba25284e4a02b21725d6751d12846a2f8f33
Zou, Newton Xing <gentleman000000@hotmail.com>
2017-12-05 09:15:59 +0000

    update package

--- src/AppWrapper/MaterialUIWrapper.js
+++ src/AppWrapper/MaterialUIWrapper.js
@@ -2,16 +2,6 @@ import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiTheme from '../palettes/standard';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-
-// Ensure that the tap event plugin is not injected multiple times.
-// If this happens, then there would be a runtime error on the page.
-
-try {
-	injectTapEventPlugin();
-} catch (ex) {
-
-}
 
 const MaterialUIWrapper = props => {
 	return (
